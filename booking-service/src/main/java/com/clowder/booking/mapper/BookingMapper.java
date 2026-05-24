6aefb77438f7c677183554910b12016f9e999b63
ClowderDev <tranlevinhbuu@gmail.com>
2026-05-24 17:04:09 +0700

    fix: fix booking mapper

--- booking-service/src/main/java/com/clowder/booking/mapper/BookingMapper.java
+++ booking-service/src/main/java/com/clowder/booking/mapper/BookingMapper.java
@@ -5,15 +5,14 @@ import com.clowder.booking.model.Booking;
 
 public class BookingMapper {
   public static BookingDTO toDto(Booking booking) {
-    BookingDTO bookingDTO = new BookingDTO();
-    bookingDTO.setId(booking.getId());
-    bookingDTO.setCustomerId(booking.getCustomerId());
-    bookingDTO.setStatus(booking.getStatus());
-    bookingDTO.setEndTime(booking.getEndTime());
-    bookingDTO.setStartTime(booking.getStartTime());
-    bookingDTO.setSalonId(booking.getSalonId());
-    bookingDTO.setServiceIds(booking.getServiceIds());
-    bookingDTO.setTotalPrice(booking.getTotalPrice());
-    return bookingDTO;
+    BookingDTO dto = new BookingDTO();
+    dto.setId(booking.getId());
+    dto.setSalonId(booking.getSalonId());
+    dto.setStatus(booking.getStatus());
+    dto.setStartTime(booking.getStartTime());
+    dto.setEndTime(booking.getEndTime());
+    dto.setServiceIds(booking.getServiceIds());
+    dto.setTotalPrice(booking.getTotalPrice());
+    return dto;
   }
 }
